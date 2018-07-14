9100ef47ee12f60a2b5550d259ac2e6dc4eb0ee4
大鹏 <278334996@qq.com>
2018-07-14 14:30:28 +0800

    add login bg

---
+++ Assets/ResFormatter/AutoImageFile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Xml;
+using System.IO;
+
+
+public class AutoImageFile : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        //自动设置为Sprite2D类型 
+        TextureImporter textureImporter = (TextureImporter)assetImporter;
+        textureImporter.textureType = TextureImporterType.Sprite;
+    }
+   
+}
