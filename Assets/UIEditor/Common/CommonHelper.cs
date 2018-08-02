468eff7404fa5253d5a0d3e5933257b4c1cc5573
大鹏 <278334996@qq.com>
2018-08-02 14:23:52 +0800

    add UIEditor

---
+++ Assets/UIEditor/Common/CommonHelper.cs
@@ -0,0 +1,135 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using System.Diagnostics;
+using System;
+
+namespace U3DExtends {
+    public class CommonHelper {
+        //生成parent下的唯一控件名
+        public static string GenerateUniqueName(GameObject parent, string type)
+        {
+            var widgets = parent.GetComponentsInChildren<RectTransform>();
+            int test_num = 1;
+            string test_name = type+"_"+test_num;
+            RectTransform uiBase = null;
+            int prevent_death_count = 0;//防止死循环
+            do {
+                test_name = type+"_"+test_num;
+                uiBase = System.Array.Find(widgets, p => p.gameObject.name==test_name);
+                test_num = test_num + UnityEngine.Random.Range(1, (prevent_death_count+1)*2);
+                if (prevent_death_count++ >= 100)
+                    break;
+            } while (uiBase != null);
+        
+            return test_name;
+        }
+
+        static public bool IsPointInRect(Vector3 mouse_abs_pos, RectTransform trans)
+        {
+            if (trans != null)
+            {
+                float l_t_x = trans.position.x;
+                float l_t_y = trans.position.y;
+                float r_b_x = l_t_x + trans.sizeDelta.x;
+                float r_b_y = l_t_y - trans.sizeDelta.y;
+                if (mouse_abs_pos.x >= l_t_x && mouse_abs_pos.y <= l_t_y && mouse_abs_pos.x <= r_b_x && mouse_abs_pos.y >= r_b_y)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static Color StringToColor(string hexString)
+        {
+            int start_index = 2;
+            if (hexString.Length == 8)
+                start_index = 2;
+            else if (hexString.Length == 6)
+                start_index = 0;
+            byte r = byte.Parse(hexString.Substring(start_index, 2), System.Globalization.NumberStyles.HexNumber);
+            byte g = byte.Parse(hexString.Substring(start_index+2, 2), System.Globalization.NumberStyles.HexNumber);
+            byte b = byte.Parse(hexString.Substring(start_index+4, 2), System.Globalization.NumberStyles.HexNumber);
+            return new Color(r / 255.0f, g / 255.0f, b / 255.0f);
+        }
+
+        public static string ColorToString(Color color)
+        {
+            string hexString = "";
+            string color_hex = "00";
+            color_hex = String.Format("{0:x}", (int)Mathf.Floor(color.r * 255));
+            if (color_hex.Length < 2)
+                color_hex = "0" + color_hex;
+            hexString = hexString + color_hex;
+            color_hex = String.Format("{0:x}", (int)Mathf.Floor(color.g * 255));
+            if (color_hex.Length < 2)
+                color_hex = "0" + color_hex;
+            hexString = hexString + color_hex;
+            color_hex = String.Format("{0:x}", (int)Mathf.Floor(color.b * 255));
+            if (color_hex.Length < 2)
+                color_hex = "0" + color_hex;
+            hexString = hexString + color_hex;
+            //UnityEngine.Debug.Log("hexString :" + hexString);
+            return hexString;
+        }
+
+        public static Process ProcessCommand(string command, string argument)
+        {
+            UnityEngine.Debug.Log(string.Format("command : {0} argument{1}", command, argument));
+            ProcessStartInfo start = new ProcessStartInfo(command);
+            start.Arguments = argument;
+            start.CreateNoWindow = true;
+            start.ErrorDialog = true;
+            start.UseShellExecute = false;
+
+            if (start.UseShellExecute)
+            {
+                start.RedirectStandardOutput = false;
+                start.RedirectStandardError = false;
+                start.RedirectStandardInput = false;
+            }
+            else
+            {
+                start.RedirectStandardOutput = true;
+                start.RedirectStandardError = true;
+                start.RedirectStandardInput = true;
+                start.StandardOutputEncoding = System.Text.UTF8Encoding.UTF8;
+                start.StandardErrorEncoding = System.Text.UTF8Encoding.UTF8;
+            }
+
+            Process p = Process.Start(start);
+            //string output = p.StandardOutput.ReadToEnd();
+            //p.WaitForExit();
+            return p;
+        }
+
+        //获取文件名
+        public static string GetFileNameByPath(string path)
+        {
+            path = path.Replace("\\", "/");
+            int last_gang_index = path.LastIndexOf("/");
+            if (last_gang_index == -1)
+                return "";
+            last_gang_index++;
+            string name = path.Substring(last_gang_index, path.Length - last_gang_index);
+            int last_dot_index = name.LastIndexOf('.');
+            if (last_dot_index == -1)
+                return "";
+            name = name.Substring(0, last_dot_index);
+            return name;
+        }
+
+        //获取文件类型后缀
+        public static string GetFileTypeSuffixByPath(string path)
+        {
+            path = path.Replace("\\", "/");
+            int last_dot_index = path.LastIndexOf('.');
+            if (last_dot_index == -1)
+                return "";
+            last_dot_index++;
+            string type_str = path.Substring(last_dot_index, path.Length - last_dot_index);
+            return type_str;
+        }
+    }
+}
+#endif
---
+++ Assets/UIEditor/Common/ContextMenu.cs
@@ -0,0 +1,201 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+namespace U3DExtends {
+static public class ContextMenu
+{
+    static List<string> mEntries = new List<string>();
+    static GenericMenu mMenu;
+
+    static public void AddItem(string item, bool isChecked, GenericMenu.MenuFunction callback)
+    {
+        if (callback != null)
+        {
+            if (mMenu == null) mMenu = new GenericMenu();
+            int count = 0;
+
+            for (int i = 0; i < mEntries.Count; ++i)
+            {
+                string str = mEntries[i];
+                if (str == item) ++count;
+            }
+            mEntries.Add(item);
+
+            if (count > 0) item += " [" + count + "]";
+            mMenu.AddItem(new GUIContent(item), isChecked, callback);
+        }
+        else AddDisabledItem(item);
+    }
+
+    static public void AddItemWithArge(string item, bool isChecked, GenericMenu.MenuFunction2 callback, object arge)
+    {
+        if (callback != null)
+        {
+            if (mMenu == null) mMenu = new GenericMenu();
+            int count = 0;
+
+            for (int i = 0; i < mEntries.Count; ++i)
+            {
+                string str = mEntries[i];
+                if (str == item) ++count;
+            }
+            mEntries.Add(item);
+
+            if (count > 0) item += " [" + count + "]";
+            mMenu.AddItem(new GUIContent(item), isChecked, callback, arge);
+        }
+        else AddDisabledItem(item);
+    }
+
+        static public void Show()
+    {
+        if (mMenu != null)
+        {
+            mMenu.ShowAsContext();
+            mMenu = null;
+            mEntries.Clear();
+        }
+    }
+
+    //增加几种对齐菜单
+    static public void AddAlignMenu()
+    {
+        AddItem("对齐/左对齐 ←", false, AlignTool.AlignInHorziontalLeft);
+        AddItem("对齐/右对齐 →", false, AlignTool.AlignInHorziontalRight);
+        AddItem("对齐/上对齐 ↑", false, AlignTool.AlignInVerticalUp);
+        AddItem("对齐/下对齐 ↓", false, AlignTool.AlignInVerticalDown);
+        AddItem("对齐/水平均匀 |||", false, AlignTool.UniformDistributionInHorziontal);
+        AddItem("对齐/垂直均匀 ☰", false, AlignTool.UniformDistributionInVertical);
+        AddItem("对齐/一样大 ■", false, AlignTool.ResizeMax);
+        AddItem("对齐/一样小 ●", false, AlignTool.ResizeMin);
+    }
+
+    //增加层次菜单
+    static public void AddPriorityMenu()
+    {
+        AddItem("层次/最里层 ↑↑↑", false, PriorityTool.MoveToTopWidget);
+        AddItem("层次/最外层 ↓↓↓", false, PriorityTool.MoveToBottomWidget);
+        AddItem("层次/往里挤 ↑", false, PriorityTool.MoveUpWidget);
+        AddItem("层次/往外挤 ↓", false, PriorityTool.MoveDownWidget);
+    }
+
+    //增加UI控件菜单
+    static public void AddUIMenu()
+    {
+        AddItem("添加控件/Empty", false, UIEditorHelper.CreateEmptyObj);
+        AddItem("添加控件/Image", false, UIEditorHelper.CreateImageObj);
+        AddItem("添加控件/RawImage", false, UIEditorHelper.CreateRawImageObj);
+        AddItem("添加控件/Button", false, UIEditorHelper.CreateButtonObj);
+        AddItem("添加控件/Text", false, UIEditorHelper.CreateTextObj);
+        AddItem("添加控件/HScroll", false, UIEditorHelper.CreateHScrollViewObj);
+        AddItem("添加控件/VScroll", false, UIEditorHelper.CreateVScrollViewObj);
+    }
+
+    //增加UI组件菜单
+    static public void AddUIComponentMenu()
+    {
+        AddItem("添加组件/Image", false, UIEditorHelper.AddImageComponent);
+        //AddItem("添加组件/RawImage", false, UIEditorHelper.CreateRawImageObj);
+        //AddItem("添加组件/Button", false, UIEditorHelper.CreateButtonObj);
+        //AddItem("添加组件/Text", false, UIEditorHelper.CreateTextObj);
+        AddItem("添加组件/HLayout", false, UIEditorHelper.AddHorizontalLayoutComponent);
+        AddItem("添加组件/VLayout", false, UIEditorHelper.AddVerticalLayoutComponent);
+        AddItem("添加组件/GridLayout", false, UIEditorHelper.AddGridLayoutGroupComponent);
+            
+    }
+
+    //增加显示隐藏菜单
+    static public void AddShowOrHideMenu()
+    {
+        bool hasHideWidget = false;
+        foreach (var item in Selection.gameObjects)
+        {
+            if (!item.activeSelf)
+            {
+                hasHideWidget = true;
+                break;
+            }
+        }
+        if (hasHideWidget)
+            AddItem("显示", false, UILayoutTool.ShowAllSelectedWidgets);
+        else
+            AddItem("隐藏", false, UILayoutTool.HideAllSelectedWidgets);
+    }
+
+    static public void AddCommonItems(GameObject[] targets)
+    {
+            if (targets == null || targets.Length <= 0)
+            {
+                AddItem("新建", false, UIEditorHelper.CreatNewLayoutForMenu);
+                AddItem("打开界面", false, UIEditorHelper.LoadLayout);
+                AddItem("打开文件夹", false, UIEditorHelper.LoadLayoutWithFolder);
+            }
+            if (targets != null && targets.Length > 0)
+            {
+                AddItem("保存", false, UIEditorHelper.SaveLayoutForMenu);
+                AddItem("另存为", false, UIEditorHelper.SaveAnotherLayoutContextMenu);
+                AddItem("重新加载", false, UIEditorHelper.ReLoadLayoutForMenu);
+
+                AddSeparator("///");
+                AddItem("复制选中控件名", false, UIEditorHelper.CopySelectWidgetName);
+
+                //如果选中超过1个节点的话
+                if (targets.Length > 1)
+                {
+                    AddAlignMenu();
+                    AddItem("同流合污", false, UILayoutTool.MakeGroup);
+                }
+                AddSeparator("///");
+                if (targets.Length == 1)
+                {
+                    AddUIMenu();
+                    AddUIComponentMenu();
+                    AddPriorityMenu();
+
+                    if (UIEditorHelper.IsNodeCanDivide(targets[0]))
+                        AddItem("分道扬镖", false, UILayoutTool.UnGroup);
+                    Decorate uiBase = targets[0].GetComponent<Decorate>();
+                    if (uiBase != null)
+                    {
+                        if (uiBase.gameObject.hideFlags == HideFlags.NotEditable)
+                        {
+                            AddItem("解锁", false, UIEditorHelper.UnLockWidget);
+                        }
+                        else
+                        {
+                            AddItem("锁定", false, UIEditorHelper.LockWidget);
+                        }
+                    }
+                }
+
+                AddShowOrHideMenu();
+
+                AddSeparator("///");
+
+                AddItem("添加参考图", false, UIEditorHelper.CreateDecorate);
+                if (targets.Length == 1 && targets[0].transform.childCount > 0)
+                    AddItem("优化层级", false, UILayoutTool.OptimizeBatchForMenu);
+
+            }
+            AddItem("排序所有界面", false, UILayoutTool.ResortAllLayout);
+            AddItem("清空界面", false, UIEditorHelper.ClearAllCanvas);
+        }
+
+    static public void AddSeparator(string path)
+    {
+        if (mMenu == null) mMenu = new GenericMenu();
+
+        if (Application.platform != RuntimePlatform.OSXEditor)
+            mMenu.AddSeparator(path);
+    }
+
+    static public void AddDisabledItem(string item)
+    {
+        if (mMenu == null) mMenu = new GenericMenu();
+        mMenu.AddDisabledItem(new GUIContent(item));
+    }
+}
+}
+#endif
