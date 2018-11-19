3f82a5906c93961230651873e7af593e7d6e4ccf
大鹏 <278334996@qq.com>
2018-11-19 19:59:24 +0800

    lua_ecs

---
+++ EntityManager.lua
@@ -0,0 +1,62 @@
+local Entity = {
+	
+}
+local EntityManager = {
+	
+}
+
+function EntityManager:CreateEntity( com_type )
+	
+end
+
+function EntityManager:DestroyEntity( entity )
+	
+end
+
+function EntityManager:Exists( entity )
+	
+end
+
+function EntityManager:HasComponent( entity, com_type )
+	
+end
+
+function EntityManager:Instantiate( srcEntity )
+	
+end
+
+function EntityManager:AddComponent( entity, com_type )
+	
+end
+
+function EntityManager:RemoveComponent( entity, com_type )
+	
+end
+
+function EntityManager:AddComponentData( entity, com_data )
+	
+end
+
+function EntityManager:SetComponentData( entity, com_data )
+	
+end
+
+function EntityManager:GetComponentData( entity )
+	
+end
+
+function EntityManager:GetAllEntities(  )
+	
+end
+
+function EntityManager:GetComponentTypes( entity )
+	
+end
+
+function EntityManager:GetComponentCount( entity )
+	
+end
+
+
+
+return EntityManager
