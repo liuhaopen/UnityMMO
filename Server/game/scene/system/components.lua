cb852d14db49cf9681a0a5bec9bba8c1378de08f
大鹏 <278334996@qq.com>
2019-05-13 22:50:18 +0800

    ecs with pure lua

--- Server/game/scene/system/components.lua
+++ Server/game/scene/system/components.lua
@@ -1,14 +1,14 @@
 local ECS = require "ECS"
 
-ECS.TypeManager.RegisterType("umo.position", {x="integer", y="integer", z="integer"})
-ECS.TypeManager.RegisterType("umo.target_pos", {x="integer", y="integer", z="integer"})
-ECS.TypeManager.RegisterType("umo.uid", {value="integer"})
-ECS.TypeManager.RegisterType("umo.type_id", {value="integer"})--Role类型的话对应RoleID，Monster和NPC类型对应TypeID
-ECS.TypeManager.RegisterType("umo.scene_obj_type", {value="integer"})
-ECS.TypeManager.RegisterType("umo.hp", {cur="integer", max="integer"})
-ECS.TypeManager.RegisterType("umo.move_speed", {value="integer"})
-ECS.TypeManager.RegisterType("umo.patrol_info", {x="integer", y="integer", z="integer", radius="integer"})--怪物有一个中心点，以此点划圆巡逻
-ECS.TypeManager.RegisterType("umo.aoi_handle", {value="integer"})
-ECS.TypeManager.RegisterType("umo.damage_event", {id="integer"})
-ECS.TypeManager.RegisterType("umo.monster_ai", {ai_id="integer"})
-ECS.TypeManager.RegisterType("umo.monster_state", {state="integer", sub_state="integer"})
+ECS.TypeManager.RegisterType("umo.position", {x=0, y=0, z=0})
+ECS.TypeManager.RegisterType("umo.target_pos", {x=0, y=0, z=0})
+ECS.TypeManager.RegisterType("umo.uid", {value=0})
+ECS.TypeManager.RegisterType("umo.type_id", {value=0})--Role类型的话对应RoleID，Monster和NPC类型对应TypeID
+ECS.TypeManager.RegisterType("umo.scene_obj_type", {value=0})
+ECS.TypeManager.RegisterType("umo.hp", {cur=0, max=0})
+ECS.TypeManager.RegisterType("umo.move_speed", {value=0})
+ECS.TypeManager.RegisterType("umo.patrol_info", {x=0, y=0, z=0, radius=0})--怪物有一个中心点，以此点划圆巡逻
+ECS.TypeManager.RegisterType("umo.aoi_handle", {value=0})
+ECS.TypeManager.RegisterType("umo.damage_event", {id=0})
+ECS.TypeManager.RegisterType("umo.monster_ai", {ai_id=0})
+ECS.TypeManager.RegisterType("umo.monster_state", {state=0, sub_state=0})
